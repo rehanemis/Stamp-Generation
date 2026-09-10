d8c19d908a5ec9e7c75993c28a211ac9a367a6f4
rehanemis <rehanemis@gmail.com>
2026-09-10 17:15:14 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -500,15 +500,14 @@ with col_right:
         icon_scale_mode=icon_scale_mode
     )
 
-    #st.image(stamp_img, use_container_width=True)
-     st.info("Please contact Muhammad Rehan Baig.")
-    #buf = io.BytesIO()
-    #stamp_img.save(buf, format="PNG", dpi=(300, 300))
-    #byte_im = buf.getvalue()
+    st.image(stamp_img, use_container_width=True)
+    buf = io.BytesIO()
+    stamp_img.save(buf, format="PNG", dpi=(300, 300))
+    byte_im = buf.getvalue()
 
-    #st.download_button(
-       # label="💾 Download High-Res PNG (300 DPI)",
-        #data=byte_im,
-        #file_name="official_stamp_high_res.png",
-        #mime="image/png"
+    st.download_button(
+        label="💾 Download High-Res PNG (300 DPI)",
+        data=byte_im,
+        file_name="official_stamp_high_res.png",
+        mime="image/png"
     )
